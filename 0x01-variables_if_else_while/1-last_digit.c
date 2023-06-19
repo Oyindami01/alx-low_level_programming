cd429c469ccb2e62a5755a5a097d88ab15b17453
Oyindami01 <olapejualliyu@gmail.com>
2023-06-19 03:46:04 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,9 +6,10 @@
  * main - Entry point
  *
  * Description: print the value of n status:
- *              greater than, is zero and is less than 6.
+ *              greater than, is zero and is not less than 6.
+ *
  * Return: 0 (Success)
- */
+*/
 
 int main(void)
 {
